e2be323875236a5c89ec8b6092ef5adc0e28838e
rabiulrafi <rabiulrafi01@gmail.com>
2020-06-10 21:30:58 +0600

    'change'

--- config/settings.py
+++ config/settings.py
@@ -126,4 +126,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
